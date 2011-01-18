ce5e3363e7f19ad2fb3087c7f6c8d32f1de5f6db
Karl Moskowski <devnull@localhost>
2011-01-18 12:49:07 -0500

    Fix #2 - ignore Mac App Store purchases

--- FindSparkleBundlesOperation.m
+++ FindSparkleBundlesOperation.m
@@ -8,7 +8,7 @@ extern void _LSCopyAllApplicationURLs(NSArray **);
 
 - (void) main {
 	NSFileManager *fm = [NSFileManager new];
-
+	
 	// assemble a list of typical bundle locations
 	NSMutableArray *otherBundleDirs = [NSMutableArray array];
 	NSString *dir = nil;
@@ -21,11 +21,11 @@ extern void _LSCopyAllApplicationURLs(NSArray **);
 		if ([fm fileExistsAtPath:dir])
 			[otherBundleDirs addObject:dir];
 	}
-
+	
 	// find all the applications on the system
 	NSMutableArray *bundleURLs;
 	_LSCopyAllApplicationURLs(&bundleURLs);
-
+	
 	// find all bundles in the bundle locations
 	for (dir in otherBundleDirs) {
 		NSArray *dirContents = [fm contentsOfDirectoryAtPath:dir error:nil];
@@ -35,16 +35,19 @@ extern void _LSCopyAllApplicationURLs(NSArray **);
 				[bundleURLs addObject:[NSURL fileURLWithPath:path]];
 		}
 	}
-
-	// check all the found bundles & apps to see if they're Sparkle-enabled
+	
+	// check all the found bundles & apps to see if they're Sparkle-enabled, ignoring Mac App Store purchases (in case the developer didn't clean up Info.plist)
 	if ([bundleURLs count] > 0) {
 		for (NSURL *bundleURL in bundleURLs) {
 			NSBundle *bundle = [NSBundle bundleWithURL:bundleURL];
-			NSString *suFeedURL = [[bundle infoDictionary] objectForKey:@"SUFeedURL"];
-			if (suFeedURL && [[NSApp delegate] respondsToSelector:@selector(checkAppUpdateForBundleURL:)]) {
-				[[NSApp delegate] performSelectorOnMainThread:@selector(checkAppUpdateForBundleURL:)
-												   withObject:bundleURL
-												waitUntilDone:NO];
+			NSString *masReceiptPath = [[[bundleURL path] stringByAppendingPathComponent:@"Contents"] stringByAppendingPathComponent:@"_MASReceipt"];
+			if (![fm fileExistsAtPath:masReceiptPath]) {
+				NSString *suFeedURL = [[bundle infoDictionary] objectForKey:@"SUFeedURL"];
+				if (suFeedURL && [[NSApp delegate] respondsToSelector:@selector(checkAppUpdateForBundleURL:)]) {
+					[[NSApp delegate] performSelectorOnMainThread:@selector(checkAppUpdateForBundleURL:)
+													   withObject:bundleURL
+													waitUntilDone:NO];
+				}
 			}
 		}
 	}
